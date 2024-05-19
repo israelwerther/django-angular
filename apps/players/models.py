182671b22c0063175e1ea9cd2c04bd804213ff75
israelwerther <israelwerther48@outlook.com>
2024-05-19 17:27:38 -0300

    fix: remove field lastupdate

--- apps/players/models.py
+++ apps/players/models.py
@@ -3,8 +3,7 @@ from django.db import models
 class Player(models.Model):
     name = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
-    email = models.EmailField()
-    last_update = models.DateTimeField(null=True, blank=True)
+    email = models.EmailField()    
 
     def __str__(self):
         return self.name
