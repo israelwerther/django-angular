4164e2f501e81a4ddacf409f0b4959f2e0ad4c19
israelwerther <israelwerther48@outlook.com>
2024-05-22 17:16:57 -0300

    cleanup: unnecessary parts

--- apps/players/models.py
+++ apps/players/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 from apps.resources.models import Iron
 
 class Player(models.Model):
