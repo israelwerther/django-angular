528d98fd31d5475bf5d74834e60cb58b19d35792
israelwerther <israelwerther48@outlook.com>
2024-05-23 08:46:14 -0300

    feat: add apps common

---
+++ apps/common/models.py
@@ -0,0 +1,13 @@
+from django_lifecycle import LifecycleModelMixin
+
+from django.db import models
+
+import uuid
+
+class BaseModel(LifecycleModelMixin, models.Model):
+	# id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+	created_at  = models.DateTimeField(verbose_name='Created at', auto_now_add=True)
+	updated_at = models.DateTimeField(verbose_name='Updated at', auto_now=True)
+
+	class Meta:
+		abstract = True
