1cb96d902f9154820499a8546d48e296b1d36075
israelwerther <israelwerther48@outlook.com>
2024-08-10 15:55:57 -0300

    clenaup: remove commented-out code

--- apps/players/serializers.py
+++ apps/players/serializers.py
@@ -11,26 +11,3 @@ class PlayerSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'name', 'username', 'email', 'created_at', 'updated_at', 'user', 'urls', 'planets'
         )
-
-    
-
-    # def create(self, validated_data):
-    #     iron_data = validated_data.pop('iron', None)
-    #     player = Player.objects.create(**validated_data)
-
-    #     if iron_data is not None:
-    #         iron = Iron.objects.create(**iron_data)
-    #         player.iron = iron
-    #         player.save()
-
-    #     return player
-
-    # def to_representation(self, instance):
-    #     representation = super(PlayerSerializer, self).to_representation(instance)
-
-    #     if instance.iron is not None:
-    #         representation['iron'] = IronSerializer(instance.iron).data
-    #     else:
-    #         representation['iron'] = None
-
-    #     return representation
