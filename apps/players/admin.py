8fe41f5e0c56ca2a82a3672bfda641016dad6080
israelwerther <israelwerther48@outlook.com>
2024-08-10 23:32:25 -0300

    feat: add PlanetInLine

--- apps/players/admin.py
+++ apps/players/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
+from apps.planets.models import Planet
+
 from .models import Player
 
-admin.site.register(Player)
+
+class PlanetInline(admin.TabularInline):
+    model = Planet
+    extra = 1  # Número de formulários em branco para adicionar novos planetas
+
+class PlayerAdmin(admin.ModelAdmin):
+    inlines = [PlanetInline]
+
+admin.site.register(Player, PlayerAdmin)
