8551f03c0b140203c6fd13d5b23682137db9ce57
israelwerther <israelwerther48@outlook.com>
2024-08-21 06:17:18 -0300

    test: fixed-plugin

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -5,12 +5,12 @@ from apps.players.serializers import PlayerSerializer
 from apps.planets.serializers import PlanetSerializer
 
 class UserSerializer(serializers.ModelSerializer):
-    urls = serializers.JSONField()
+    # urls = serializers.JSONField()
     planets = PlanetSerializer(many=True, read_only=True) 
     current_player = PlayerSerializer(read_only=True) 
 
     class Meta:
         model = User
         fields = (
-            'id', 'current_player', 'planets', 'urls'
+            'id', 'current_player', 'planets'
         )
