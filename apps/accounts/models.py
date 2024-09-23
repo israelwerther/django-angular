00153ce736c40cdc1f17c9745bce7b4bd2697a53
israelwerther <israelwerther48@outlook.com>
2024-09-23 13:43:01 -0300

    feat: select planet correctly

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,8 +2,6 @@ import uuid
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
-
-from apps.planets.models import Planet
 from apps.players.models import Player
 
 class User(AbstractUser):
