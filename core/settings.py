5249af6941b68267e5a3dd10cc59fae881c0eae9
israelwerther <israelwerther48@outlook.com>
2024-09-28 11:12:45 -0300

    fix: correct url on logout settings.py

--- core/settings.py
+++ core/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'core.urls'
 LOGIN_REDIRECT_URL = "home"  # Route defined in home/urls.py
-LOGOUT_REDIRECT_URL = "home"  # Route defined in home/urls.py
+LOGOUT_REDIRECT_URL = "/"  # Route defined in home/urls.py
 TEMPLATE_DIR = os.path.join(CORE_DIR, "apps/templates")  # ROOT dir for templates
 
 TEMPLATES = [
