ada4b56c229b3aaaa8f6ebeb92dcb1a18574c27a
israelwerther <israelwerther48@outlook.com>
2024-09-28 11:11:44 -0300

    fix: correct user model in SingupForm

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -5,8 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
+from apps.accounts.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField(
