d44b1476bded91dbf55794046ca2a8684ab73dd0
israelwerther <israelwerther48@outlook.com>
2024-09-28 21:07:03 -0300

    refact: fixes relationships

--- apps/planets/models.py
+++ apps/planets/models.py
@@ -1,22 +1,18 @@
 from django.db import models
 from django.urls import reverse
 from apps.common.models import BaseModel
-# from apps.players.models import Player
-from apps.resources.models import Iron
 from django_lifecycle import hook
 
 class Planet(BaseModel):
     player = models.ForeignKey('players.Player', on_delete=models.CASCADE, related_name='planets')
     name = models.CharField(max_length=100)
     home_planet = models.BooleanField(default=False)
-    iron = models.OneToOneField(Iron, on_delete=models.CASCADE, related_name='planet_iron', null=True, blank=True)
 
     @hook('after_save')
     def create_iron_for_planet(instance, **kwargs):
-        if instance.pk is not None and instance.iron is None:
-            iron_obj = Iron.objects.create()
-            instance.iron = iron_obj
-            instance.save()
+        if instance.pk is not None and not hasattr(instance, 'iron'):
+            from apps.resources.models import Iron
+            Iron.objects.create(planet=instance)
 
     def __str__(self):
         return self.name
--- apps/resources/models.py
+++ apps/resources/models.py
@@ -1,28 +1,27 @@
-import datetime
 from django.db import models
 from apps.common.models import BaseModel
 from django_lifecycle import hook
+import datetime
 
 class IronMine(BaseModel):
+    iron = models.OneToOneField('Iron', on_delete=models.CASCADE, related_name='mine')
     level = models.IntegerField(default=0)
 
     def __str__(self):
         return str(self.id)
 
 class Iron(BaseModel):
+    planet = models.OneToOneField('planets.Planet', on_delete=models.CASCADE, related_name='iron')
     quantity = models.IntegerField(default=0)
-    mine = models.OneToOneField(IronMine, on_delete=models.CASCADE, null=True, blank=True)
 
     @hook('after_save')
-    def create_mine(instance, **kwargs):
-        if instance.pk is not None and instance.mine is None:
-            mine_obj = IronMine.objects.create()
-            instance.mine = mine_obj
-            instance.save()
-    
+    def create_mine_for_iron(instance, **kwargs):
+        if instance.pk is not None and not hasattr(instance, 'mine'):
+            IronMine.objects.create(iron=instance)
+
     def __str__(self):
         return str(self.id)
-    
+
     def get_current_iron(self):
         if not self:
             return 0
@@ -32,3 +31,4 @@ class Iron(BaseModel):
         generated_iron = int(time_diff.total_seconds() * iron_per_second)
 
         return self.quantity + generated_iron
+
