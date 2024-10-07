24f2f756c72b7c4d33013eb110f7b397b744ab8c
israelwerther <israelwerther48@outlook.com>
2024-10-07 13:28:13 -0300

    feat: adjusting mine update logic

--- apps/resources/views.py
+++ apps/resources/views.py
@@ -17,7 +17,18 @@ class IronMineModelViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['get'])
     def update_mine(self, request, pk=None):
         iron_mine = self.get_object()
-        print('iron_mine::: ', iron_mine)
-        
+        iron = iron_mine.planet.iron
+        current_iron = iron.get_current_iron()
+        required_iron = iron_mine.resources_required_for_upgrade()
+
+        if current_iron >= required_iron:
+            iron_mine.level += 1
+            iron_mine.save()
+            
+
         serializer = self.get_serializer(iron_mine)
-        return Response(serializer.data)
+        response_data = serializer.data
+        response_data['current_iron'] = current_iron
+        response_data['resources_required_for_upgrade'] = required_iron
+
+        return Response(response_data)
