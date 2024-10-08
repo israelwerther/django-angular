83fe1a64ef605114efb416db731d0be15a560155
israelwerther <israelwerther48@outlook.com>
2024-10-08 13:43:32 -0300

    feat:  correctly shows upgrade duration

--- apps/resources/serializers.py
+++ apps/resources/serializers.py
@@ -13,9 +13,25 @@ class IronMineSerializer(serializers.ModelSerializer):
             'urls',
             'upgrade_duration'
         )
+
     def get_upgrade_duration(self, obj):
-        duration = obj.upgrade_duration()
-        return str(duration)
+        duration = obj.upgrade_duration() 
+        total_seconds = int(duration.total_seconds())
+
+        days, remainder = divmod(total_seconds, 86400)
+        hours, remainder = divmod(remainder, 3600)
+        minutes, seconds = divmod(remainder, 60)
+
+        duration_str = ""
+        if days > 0:
+            duration_str += f"{days}d "
+        if hours > 0 or days > 0:
+            duration_str += f"{hours}h "
+        
+        duration_str += f"{minutes}m {seconds}s"
+
+        return duration_str.strip()
+
 
 class IronSerializer(serializers.ModelSerializer):
     current_iron = serializers.SerializerMethodField()
